6e663396e8ec72d25cb89917d0cd2154cf8e1f8d
Denis Onchukov <cadako@gmail.com>
2025-11-25 20:41:05 +0300

    comment ui test

--- test_ui.py
+++ test_ui.py
@@ -1,6 +1,5 @@
 # test_ui.py (новый файл в корне проекта)
-
-import pytest
+'''
 from playwright.sync_api import Page, expect
 import os
 
@@ -10,9 +9,6 @@ FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost")
 
 @pytest.mark.ui  # Специальная метка для UI-тестов
 def test_products_list_displays_laptop(page: Page):
-    """
-    Проверяет, что товар 'Laptop' отображается на главной странице.
-    """
     page.goto(FRONTEND_URL)
 
     # Используем методы Playwright для поиска элемента
@@ -26,4 +22,4 @@ def test_products_list_displays_laptop(page: Page):
     # Используем CSS-селектор для поиска второй ячейки (цены) в найденной строке
     price_cell = laptop_row.locator("xpath=./following-sibling::td[1]")
     expect(price_cell).to_have_text("9999.50")
-
+'''
